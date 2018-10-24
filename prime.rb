b79354ebecc73997dcea2e87300bdab069893284
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-24 18:23:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
-def prime?(num)
-  if num <= 1
-    false
-  elsif Math.sqrt(num).to_i.downto(2).each {|i|} && i == 0
-  false 
-else 
-  true
+def prime?(number)
+  if number <= 1
+      return false
+    end
+  (2..Math.sqrt(number)).each do |num|
+      if number%num  == 0 
+       return false
+    end
+  end
+      return true
 end
+
